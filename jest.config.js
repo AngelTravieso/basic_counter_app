0e4c0767ea5fd32fe8a1705182895b8dbcb2b1e5
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-14 08:04:26 -0400

    pruebas con async-await

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+
+// Se hace si se tiene node < v18
+// Si se tiene cra no debe dar error, esto es para pruebas asyn
+module.exports = {
+    // TODO: jsdom,
+    setupFiles: ['./jest.setup.js']
+}
