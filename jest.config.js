aa49184c9612323d795205dc76ef6568ed26105c
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-22 08:44:01 -0400

    Pruebas en <FirstApp />
    configuraciones extras para testing

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@
 // Si se tiene cra no debe dar error, esto es para pruebas asyn
 module.exports = {
     // TODO: jsdom,
+    testEnvironment: 'jest-environment-jsdom',
     setupFiles: ['./jest.setup.js']
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp />
+    <FirstApp title="Hola, soy Vegeta" />
     {/* <CounterApp value={10} /> */}
   </React.StrictMode>
 )
